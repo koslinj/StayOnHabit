78f68e432e20670cb2e154c7823d9c6497e15468
koslinj <koslinj@gmail.com>
2023-08-27 19:57:36 +0200

    sign out button

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import SignOutButton from "./SignOutButton";
+import { getServerSession } from "next-auth/next"
+import { options } from "../api/auth/[...nextauth]/options"
 
+export default async function Navbar() {
+    const session = await getServerSession(options)
 
-export default function Navbar() {
-  return (
-    <nav className="bg-fuchsia-700">
-        <Link href='/'>Home</Link>
-        <Link href='/dashboard'>Dashboard</Link>
-    </nav>
-  )
+    return (
+        <nav className="bg-orange-500 flex justify-between text-xl font-bold p-4">
+            <div className="flex gap-8">
+                <Link href='/'>Home</Link>
+                <Link href='/dashboard'>Dashboard</Link>
+            </div>
+            {session && <SignOutButton />}
+        </nav>
+    )
 }
