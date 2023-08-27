1a0d1dd91a860ca676a7c4013b0387f52cff6b39
koslinj <koslinj@gmail.com>
2023-08-27 23:43:11 +0200

    add callback to sign out

--- app/components/SignOutButton.tsx
+++ app/components/SignOutButton.tsx
@@ -3,6 +3,6 @@ import { signOut } from "next-auth/react"
 
 export default function SignOutButton() {
     return (
-        <button onClick={() => signOut()}>Sign Out</button>
+        <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}>Sign Out</button>
     )
 }
