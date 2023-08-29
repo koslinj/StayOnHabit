1441ec365b7951d58a8855ae078b742f2b5c373e
koslinj <koslinj@gmail.com>
2023-08-30 00:01:43 +0200

    using BASE_URL from env

--- lib/database/addDay.ts
+++ lib/database/addDay.ts
@@ -1,7 +1,7 @@
 
 export async function addDay(habit_id: number, day: string) {
     try {
-        const res = await fetch('http://localhost:3000/api/days', {
+        const res = await fetch(process.env.BASE_URL + '/api/days', {
             method: 'POST',
             body: JSON.stringify({
                 habit_id,
