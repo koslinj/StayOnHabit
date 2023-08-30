0562d7d01837ade2a6ee4175a70eb3d3102091e1
koslinj <koslinj@gmail.com>
2023-08-30 12:32:01 +0200

    useOptimistic() and serv actions for better ux

--- app/components/Square.tsx
+++ app/components/Square.tsx
@@ -2,6 +2,7 @@
 import { addDay } from "@/lib/database/addDay"
 import { deleteDay } from "@/lib/database/deleteDay"
 import { useRouter } from "next/navigation"
+import { experimental_useOptimistic as useOptimistic } from "react"
 
 type Props = {
     day: string
@@ -9,22 +10,34 @@ type Props = {
     habit_id: number
 }
 
-export default async function Square({ day, state, habit_id }: Props) {
-    const router = useRouter()
-    async function handleRed(){
+export default function Square({ day, state, habit_id }: Props) {
+    const [optimisticState, setoptimisticState] = useOptimistic(state)
+    async function handleRed() {
+        setoptimisticState(true)
         await addDay(habit_id, day)
-        router.refresh()
     }
-    async function handleGreen(){
+    async function handleGreen() {
+        setoptimisticState(false)
         await deleteDay(habit_id, day)
-        router.refresh()
     }
-    
+
     return (
-        <div
-            onClick={!state ? () => handleRed() : () => handleGreen()}
-            className={`w-12 h-12 mx-2 hover:scale-110 duration-100 ${state ? 'bg-green-500 hover:bg-green-600' : 'bg-red-500 hover:bg-red-600'}`}
-        >
-        </div>
+        <>
+            {!optimisticState ?
+                <form action={handleRed}>
+                    <button
+                        className={`w-12 h-12 mx-2 hover:scale-110 duration-100 bg-red-500 hover:bg-red-600`}
+                    >
+                    </button>
+                </form>
+                :
+                <form action={handleGreen}>
+                    <button
+                        className={`w-12 h-12 mx-2 hover:scale-110 duration-100 bg-green-500 hover:bg-green-600`}
+                    >
+                    </button>
+                </form>
+            }
+        </>
     )
 }
--- lib/database/deleteDay.ts
+++ lib/database/deleteDay.ts
@@ -1,16 +1,21 @@
+"use server"
+import { options } from "@/app/api/auth/[...nextauth]/options";
+import { sql } from "@vercel/postgres";
+import { getServerSession } from "next-auth/next";
+import { revalidatePath } from "next/cache"
 
 export async function deleteDay(habit_id: number, day: string) {
+    const session = await getServerSession(options)
+
+    if (!session) {
+        throw new Error('No user logged in!')
+    }
+
+    const user_id = session.user?.email
     try {
-        const res = await fetch('/api/days', {
-            method: 'DELETE',
-            body: JSON.stringify({
-                habit_id,
-                day,
-            }),
-            headers: {
-                "content-type": "application/json",
-            },
-        })
+        if (!habit_id || !day || !user_id) throw new Error('habit_id, day and user_id are required')
+        await sql`DELETE FROM days WHERE habit_id=${habit_id} AND day=${day} AND user_id=${user_id};`
+        revalidatePath('/dashboard')
     } catch (error) {
         console.log(error)
     }
