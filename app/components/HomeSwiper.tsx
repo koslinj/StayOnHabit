636a15961a62349498992e528493a0ca1c3e6bba
koslinj <koslinj@gmail.com>
2023-09-04 14:34:21 +0200

    fix not responsive home screen

--- app/components/HomeSwiper.tsx
+++ app/components/HomeSwiper.tsx
@@ -13,7 +13,7 @@ import showing6 from "@/public/showing-6.png"
 
 export default function HomeSwiper() {
     return (
-        <div className="bg-orange-500/50 rounded-2xl mt-12">
+        <div className="bg-orange-500/50 rounded-2xl mt-12 w-[300px] sm:w-[550px] md:w-[700px] lg:w-[930px] xl:w-full">
             <Swiper
                 spaceBetween={30}
                 pagination={{
@@ -26,27 +26,27 @@ export default function HomeSwiper() {
                 centeredSlides={true}
             >
                 <SwiperSlide className="my-auto">
-                    <div className="flex justify-center pt-2 pb-8 px-12">
+                    <div className="flex justify-center pt-2 pb-8 md:px-12">
                         <Image height={500} placeholder="blur" className="border-2 border-orange-400 p-2 rounded-xl" src={showing2} alt="image showing look of website" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="my-auto">
-                    <div className="flex justify-center pt-2 pb-8 px-12">
+                    <div className="flex justify-center pt-2 pb-8 md:px-12">
                         <Image height={500} placeholder="blur" className="border-2 border-orange-400 p-2 rounded-xl" src={showing3} alt="image showing look of website" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="my-auto">
-                    <div className="flex justify-center pt-2 pb-8 px-12">
+                    <div className="flex justify-center pt-2 pb-8 md:px-12">
                         <Image height={500} placeholder="blur" className="border-2 border-orange-400 p-2 rounded-xl" src={showing4} alt="image showing look of website" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="my-auto">
-                    <div className="flex justify-center pt-2 pb-8 px-12">
+                    <div className="flex justify-center pt-2 pb-8 md:px-12">
                         <Image height={500} placeholder="blur" className="border-2 border-orange-400 p-2 rounded-xl" src={showing5} alt="image showing look of website" />
                     </div>
                 </SwiperSlide>
                 <SwiperSlide className="my-auto">
-                    <div className="flex justify-center pt-2 pb-8 px-12">
+                    <div className="flex justify-center pt-2 pb-8 md:px-12">
                         <Image height={500} placeholder="blur" className="border-2 border-orange-400 p-2 rounded-xl" src={showing6} alt="image showing look of website" />
                     </div>
                 </SwiperSlide>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HomeSwiper from "./components/HomeSwiper";
 
 export default function Home() {
     return (
-        <main className="max-w-5xl mx-auto text-center px-4">
+        <main className="max-w-5xl mx-auto text-center px-2 md:px-4">
             <h1 className="p-4 lg:p-10 font-extrabold italic text-transparent text-6xl lg:text-7xl bg-clip-text bg-gradient-to-r from-yellow-400 to-red-600">Stay On Habit</h1>
             <h2 className="text-2xl text-white font-bold">
                 A place where you can track your daily routine and become more productive day after day. Just join us and start your progress today!
@@ -16,7 +16,9 @@ export default function Home() {
             <div className="mx-auto">
                 <p className="text-white/60 italic text-lg">In order to use this app and see your dashboard you have to sign in with your github account!</p>
             </div>
-            <HomeSwiper />
+            <div className="flex justify-center">
+                <HomeSwiper />
+            </div>
         </main>
     )
 }
